6ee418feeb6bb054d55d1006f75d8e018d11576b
SergeySysoev <sergey.sysoev3010@gmail.com>
2025-07-16 16:22:20 +0300

    .

--- sandbox/decorate_me.py
+++ sandbox/decorate_me.py
@@ -1,10 +1,17 @@
 from functools import wraps
-from contextlib import ContextDecorator
-from typing import Callable, Any, TypeVar
-from neudc.utils.profile import Profile, NoProfile, get_profile, conditional_jit, toggle_jit
+
+from neudc.utils.profile import Profile
+
 
 class DetectorProfiler(Profile):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.func_name = None
-    
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            return func(*args, **kwargs)
+
+        return wrapper
