63c6c34d900ef4f52368684e6184a986e1cce14b
mgkotovanu <mgkotovanu@sberbank.ru>
2025-07-23 17:00:21 +0300

    added drop| added processing frames to drop

--- neudc/core/node/filters/mixins/hash_mixin.py
+++ neudc/core/node/filters/mixins/hash_mixin.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, ClassVar
 
 import imagehash
 from PIL import Image
+from pathlib import Path
 
 from neudc.utils import LOGGER
 
@@ -76,11 +77,11 @@ class HashFilterMixin:
         pil_image = Image.fromarray(frame.image)
         current_hash = self.hash_func(pil_image, hash_size=self.hash_size)
 
-        key_folder = frame.source_frame
+        key_folder = Path(frame.source_frame).parent.name
 
         for old_hash in self.cache[key_folder].values():
             if (current_hash - old_hash) <= self.delta:
-                return None
-
+                frame.drop = True
+                return frame
         self.cache[key_folder][frame.frame_id] = current_hash
         return frame
